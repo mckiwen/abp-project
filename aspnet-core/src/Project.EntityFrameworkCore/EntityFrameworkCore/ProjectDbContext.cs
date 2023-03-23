657b77bb40bc77f232e2feae0a3aca1971a0f8f6
Alberto <albantunez91@gmail.com>
2023-03-23 10:18:09 +0000

    Task Entity created in Core and DbContext DbSet<Task> in EF Core

--- aspnet-core/src/Project.EntityFrameworkCore/EntityFrameworkCore/ProjectDbContext.cs
+++ aspnet-core/src/Project.EntityFrameworkCore/EntityFrameworkCore/ProjectDbContext.cs
@@ -3,13 +3,15 @@ using Abp.Zero.EntityFrameworkCore;
 using Project.Authorization.Roles;
 using Project.Authorization.Users;
 using Project.MultiTenancy;
+using Project.Tasks;
 
 namespace Project.EntityFrameworkCore
 {
     public class ProjectDbContext : AbpZeroDbContext<Tenant, Role, User, ProjectDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<Task> Tasks { get; set; }
+
         public ProjectDbContext(DbContextOptions<ProjectDbContext> options)
             : base(options)
         {
