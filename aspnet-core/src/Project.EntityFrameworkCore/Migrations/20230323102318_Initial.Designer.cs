ec3440f5a73e53ab80e830b0f30df6cbdeb6df87
Alberto <albantunez91@gmail.com>
2023-03-23 11:21:24 +0000

    Initial migration performed for Task

--- aspnet-core/src/Project.EntityFrameworkCore/Migrations/20230323102318_Initial.Designer.cs
+++ aspnet-core/src/Project.EntityFrameworkCore/Migrations/20230323102318_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project.EntityFrameworkCore;
 
@@ -11,9 +12,10 @@ using Project.EntityFrameworkCore;
 namespace Project.Migrations
 {
     [DbContext(typeof(ProjectDbContext))]
-    partial class ProjectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230323102318_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
