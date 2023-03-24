0d45c72f1402afeaa3e03fd49a331e04d4945cb8
Alberto <albantunez91@gmail.com>
2023-03-24 10:37:45 +0000

    Añadidos los Dtos y ITaskAppService y TaskAppService

---
+++ aspnet-core/src/Project.Application/Tasks/ITaskAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Tasks.Dtos;
+
+namespace Project.Tasks
+{
+    public interface ITaskAppService : IApplicationService
+    {
+        Task<ListResultDto<TaskListDto>> GetAll(GetAllTasksInput input);
+    }
+}
---
+++ aspnet-core/src/Project.Application/Tasks/TaskAppService.cs
@@ -0,0 +1,37 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using AutoMapper;
+using Project.Tasks.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Linq.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Tasks
+{
+    public class TaskAppService : ProjectAppServiceBase, ITaskAppService
+    {
+        private readonly IRepository<Task> _taskRepository;
+
+        public TaskAppService(IRepository<Task> taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<ListResultDto<TaskListDto>> GetAll(GetAllTasksInput input)
+        {
+            var tasks = await _taskRepository
+                .GetAll()
+                .WhereIf(input.State.HasValue, t => t.State == input.State.Value)
+                .OrderByDescending(t => t.CreationTime)
+                .ToListAsync();
+
+            return new ListResultDto<TaskListDto>(
+                ObjectMapper.Map<List<TaskListDto>>(tasks)
+            );
+        }
+    }
+}
