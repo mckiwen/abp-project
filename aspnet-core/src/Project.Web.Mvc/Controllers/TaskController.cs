a2409db1057925e4c6be204d39701e49fb218b28
Alberto <albantunez91@gmail.com>
2023-03-27 18:30:58 +0100

    Añadida la vista, no funciona la importacion de la clase

---
+++ aspnet-core/src/Project.Web.Mvc/Controllers/TaskController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using Project.Tasks;
+using Project.Tasks.Dtos;
+using Project.Web.Models.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using Project.Controllers;
+
+
+namespace Project.Web.Controllers
+{
+    public class TasksController : ProjectControllerBase
+    {
+        private readonly ITaskAppService _taskAppService;
+
+        public TasksController(ITaskAppService taskAppService)
+        {
+            _taskAppService = taskAppService;
+        }
+
+        public async Task<ActionResult> Index(GetAllTasksInput input)
+        {
+            var output = await _taskAppService.GetAll(input);
+            var model = new IndexViewModel(output.Items);
+            return View(model);
+        }
+    }
+}
---
+++ aspnet-core/src/Project.Web.Mvc/Models/Tasks/IndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Project.Tasks.Dtos;
+using Project.Tasks;
+using System.Collections.Generic;
+
+namespace Project.Web.Models.Tasks
+{
+    public class IndexViewModel
+    {
+        public IReadOnlyList<TaskListDto> Tasks { get; }
+
+        public IndexViewModel(IReadOnlyList<TaskListDto> tasks)
+        {
+            Tasks = tasks;
+        }
+
+        public string GetTaskLabel(TaskListDto task)
+        {
+            switch (task.State)
+            {
+                case TaskState.Open:
+                    return "label-success";
+                default:
+                    return "label-default";
+            }
+        }
+    }
+}
